447b9c8e1941c3c7c124d556fe7b069109a60ebe
Kiryl Klimovich <29510036+kpklimovich@users.noreply.github.com>
2023-10-22 19:04:37 +0300

    add metdos

--- leeson2/src/main/java/org/example/Main.java
+++ leeson2/src/main/java/org/example/Main.java
@@ -1,7 +1,47 @@
 package org.example;
 
 public class Main {
+
+    public static void printThreeWords() {
+        System.out.println("Orange\n\rBanana\n\rApple\n\r");
+    }
+
+    public static void checkSumSign() {
+        int a = 10;
+        int b = -11;
+        String negativeSumWord = "Сумма отрицательная";
+        String positiveSumWord = "Сумма положительная";
+
+        if ((a + b) >= 0) {
+            System.out.println(positiveSumWord);
+        } else {
+            System.out.println(negativeSumWord);
+        }
+    }
+
+    public static void printColor() {
+        int value = 101;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if ((value > 0) && (value <= 100)) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 7;
+        int b = 7;
+
+        String result = (a >= b) ? "a >= b" : "a < b";
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        checkSumSign();
+        printColor();
+        compareNumbers();
     }
 }
