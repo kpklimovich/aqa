55592de5d13f25ebebba338f3ae2598b320188e6
Kiryl Klimovich <29510036+kpklimovich@users.noreply.github.com>
2023-10-25 22:53:13 +0300

    remove comments, fix typo

--- lesson3/src/main/java/org/example/Main.java
+++ lesson3/src/main/java/org/example/Main.java
@@ -3,87 +3,86 @@ package org.example;
 import java.util.Arrays;
 
 public class Main {
-
-    //task1
     public static boolean betweenNumbers(int a, int b) {
         return ((a + b) > 10) && ((a + b) <= 20);
     }
 
-    //task2
     public static void positiveOrNegative(int number) {
         String result = (number >= 0) ? "positive" : "negative";
         System.out.println(result);
     }
 
-    //task3
-    public static boolean doesPositiveOrNegative(int number) {
-        return (number >= 0);
+    public static boolean revertPositiveOrNegative(int number) {
+        return !(number >= 0);
     }
 
-    //task4
     public static void printStrCntTimes(String str, int cntTimes) {
         for (int i = 0; i < cntTimes; i++) {
             System.out.println(str);
         }
     }
 
-    //task5
     public static boolean doesLeapYear(int year) {
         if ((year % 100 == 0) && (year % 400) != 0) {
             return false;
-        } else if (year % 4 == 0) {
-            return true;
         } else {
-            return false;
+            return year % 4 == 0;
         }
     }
 
-    //task6
     public static void replaceNumbersInArr() {
         int[] arr = {0, 1, 0, 1, 1, 1, 0, 0};
-
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (arr[i] == 0) ? 1 : 0;
         }
+        System.out.println(Arrays.toString(arr));
     }
 
-    //task7
     public static void fillArray() {
         int[] arr = new int[100];
-
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i + 1;
         }
+        System.out.println(Arrays.toString(arr));
     }
 
-    //task8
-    public static void chngeArray() {
+    public static void changeArray() {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < 6) {
                 arr[i] *= 2;
             }
         }
+        System.out.println(Arrays.toString(arr));
     }
 
-    //task9
     public static void fillMatrix() {
-        int[][] matrix = new int[6][6];
-
+        int[][] matrix = new int[5][5];
         for (int i = 0; i < matrix.length; i++) {
             matrix[i][i] = 1;
-            matrix[i][matrix.length - i] = 1;
+            matrix[i][matrix.length - (i+1)] = 1;
+        }
+        for( int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
         }
     }
 
-    //taks10
-    public static int[] createAndFillArray(int len, int inintilaValue) {
+    public static int[] createAndFillArray(int len, int initialValue) {
         int[] array = new int[len];
-        Arrays.fill(array, inintilaValue);
+        Arrays.fill(array, initialValue);
         return array;
     }
 
     public static void main(String[] args) {
+        System.out.println(betweenNumbers(11,9));
+        positiveOrNegative(0);
+        System.out.println(revertPositiveOrNegative(-3));
+        printStrCntTimes("test",3);
+        System.out.println(doesLeapYear(400));
+        replaceNumbersInArr();
+        fillArray();
+        changeArray();
+        fillMatrix();
+        System.out.println(Arrays.toString(createAndFillArray(3,4)));
     }
 }
