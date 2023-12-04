ea0665754663ec05e510f440e47e87d6d9617156
Kiryl Klimovich <29510036+kpklimovich@users.noreply.github.com>
2023-12-04 22:44:17 +0300

    add task

--- project/src/test/java/org/example/AppTest.java
+++ project/src/test/java/org/example/AppTest.java
@@ -1,11 +1,11 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 public class AppTest {
+
     @Test
     public void testFactorial() {
-        Assertions.assertEquals(App.factorial(5), 120);
+        assertEquals(App.factorial(5), 120);
     }
 }
