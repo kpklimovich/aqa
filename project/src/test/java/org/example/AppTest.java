4410cb76227f54fd77150ee01f3ba1b781257da9
Kiryl Klimovich <29510036+kpklimovich@users.noreply.github.com>
2023-12-04 22:45:37 +0300

    remove lines

--- project/src/test/java/org/example/AppTest.java
+++ project/src/test/java/org/example/AppTest.java
@@ -2,8 +2,8 @@ package org.example;
 
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
-public class AppTest {
 
+public class AppTest {
     @Test
     public void testFactorial() {
         assertEquals(App.factorial(5), 120);
